518a6574ce97810b9065215d771be69dc2bbab83
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-25 22:29:00 -0800

    build out router config file

--- app/config/router-config.js
+++ app/config/router-config.js
@@ -1,1 +1,28 @@
 'use strict';
+
+module.exports = ['$stateProvider', '$urlRouterProvider', routerConfig];
+
+function routerConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.when('/', '/home');
+
+  let routes = [
+    {
+      name: 'home',
+      url: '/home',
+      template: require('../view/home/home.html'),
+      controller: 'HomeController',
+      controllerAs: 'homeControl',
+    },
+    {
+      name: 'register',
+      url: '/register',
+      template: require('../view/register/register.html'),
+      controller: 'RegisterController',
+      controllerAs: 'registerControl',
+    },
+  ];
+
+  routes.forEach( route => {
+    $stateProvider.state(route);
+  });
+};
