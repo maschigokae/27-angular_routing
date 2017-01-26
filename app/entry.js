e7d5eacdde5011b9bc482bb8411a9e482e11f773
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-25 22:57:13 -0800

    build out entry dependencies

--- app/entry.js
+++ app/entry.js
@@ -1,1 +1,22 @@
 'use strict';
+
+require('./scss/main.scss');
+
+const path = require('path');
+const camelcase = require('camelcase');
+const pascalcase = require('pascalcase');
+const angular = require('angular');
+const uiRouter = require('angular-ui-router');
+
+const routesApp = angular.module('routesApp', [uiRouter]);
+
+let context = require.context('./config/', true, /\.js$/);
+context.keys().forEach( fileName => {
+  routesApp.config(context(fileName));
+});
+
+context = require.context('./view/', true, /\.js$/);
+context.keys().forEach( fileName => {
+  let name = pascalcase(path.basename(fileName, '.js'));
+  routesApp.controller(name, context(fileName));
+});
