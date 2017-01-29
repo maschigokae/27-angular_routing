3b259a5abb9a00a9dad528868398435ac0aee393
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-29 13:52:45 -0800

    add home controller test to verify home title

--- test/home-controller-test.js
+++ test/home-controller-test.js
@@ -1,1 +1,20 @@
 'use strict';
+
+require('./lib/test-setup.js');
+
+const angular = require('angular');
+
+describe('home controller', function() {
+  beforeEach( () => {
+    angular.mock.module('routesApp');
+    angular.mock.inject($controller => {
+      this.homeControl = new $controller('HomeController');
+    });
+  });
+
+  describe('home controller title property', function() {
+    it('should populate h1 element with home title text', () => {
+      expect(this.homeControl.title).toEqual('Home');
+    });
+  });
+});
