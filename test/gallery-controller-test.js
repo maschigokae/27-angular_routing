22bd53a040afe466cecbf07850f022a84766d952
Caleb Sattgast <calebsattgast@gmail.com>
2017-01-30 15:54:08 -0800

    build out other component tests

--- test/gallery-controller-test.js
+++ test/gallery-controller-test.js
@@ -1,1 +1,16 @@
 'use strict';
+
+describe('gallery controller', function() {
+  beforeEach( () => {
+    angular.mock.module('routesApp');
+    angular.mock.inject($controller => {
+      this.galleryControl = new $controller('GalleryController');
+    });
+  });
+
+  describe('gallery controller title property', () => {
+    it('should populate h1 element with gallery title text', () => {
+      expect(this.galleryControl.title).toEqual('Gallery');
+    });
+  });
+});
--- test/register-controller-test.js
+++ test/register-controller-test.js
@@ -1,1 +1,16 @@
 'use strict';
+
+describe('register controller', function() {
+  beforeEach( () => {
+    angular.mock.module('routesApp');
+    angular.mock.inject($controller => {
+      this.registerControl = new $controller('RegisterController');
+    });
+  });
+
+  describe('register controller title property', () => {
+    it('should populate h1 element with register title text', () => {
+      expect(this.registerControl.title).toEqual('Register');
+    });
+  });
+});
